7e5a32c590a261d5fdc784820c516d7e3638c8a0
Muhammad Azzam <MazzamR08@gmail.com>
2022-04-06 10:19:30 +0700

    on going

--- lib/screen/detailpage.dart
+++ lib/screen/detailpage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/model/model.dart';
 import 'package:news_app/utils/utils.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class DetailPage extends StatefulWidget {
   final Article article;
@@ -37,16 +38,106 @@ class _DetailPageState extends State<DetailPage> {
                         Text(
                           widget.article.title,
                           style: tittleArticle.copyWith(fontSize: 18),
-                        )
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(Icons.account_circle),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  widget.article.author,
+                                  style:
+                                      authorDataArticle.copyWith(fontSize: 14),
+                                  overflow: TextOverflow.ellipsis,
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(Icons.calendar_today_outlined),
+                                Text(
+                                  time(DateTime.parse(
+                                      widget.article.publishedAt)),
+                                  style:
+                                      authorDataArticle.copyWith(fontSize: 14),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Text(widget.article.content,
+                            style: detailArticle.copyWith(fontSize: 16))
                       ],
                     ),
                   ),
                 )
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          offset: Offset(0,10),
+                          blurRadius: 50
+                        )
+                      ]
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back),
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              offset: Offset(0,10),
+                              blurRadius: 50
+                          )
+                        ]
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.bookmark_border_rounded),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  String time(DateTime parse) {
+    return timeago.format(parse, allowFromNow: true, locale: 'id');
   }
 }
